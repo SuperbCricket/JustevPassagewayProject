59531654b69703cd86b64bea00c20a53d69c2697
Gerben Prins <138300506+SuperbCricket@users.noreply.github.com>
2024-01-01 22:34:34 +0100

    changedSpawnLocation

--- Justev's Passageway/Assets/Scripts/Player/Movement.cs
+++ Justev's Passageway/Assets/Scripts/Player/Movement.cs
@@ -6,12 +6,6 @@ public class Movement : MonoBehaviour
 {
     public int Speed = 6;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        transform.position = new Vector3(0, 0, -1);
-    }
-
     // Update is called once per frame
     void Update()
     {
