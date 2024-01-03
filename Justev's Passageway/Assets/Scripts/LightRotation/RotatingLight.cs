254fdb696dd6ae27fcf3f10ad1c2df79c29a6f1b
Gerben Prins <138300506+SuperbCricket@users.noreply.github.com>
2024-01-03 19:01:34 +0100

    FinallyFixedScannerMovementAndRotationSpeed
    also added a rotation speed to the player light

--- Justev's Passageway/Assets/Scripts/LightRotation/RotatingLight.cs
+++ Justev's Passageway/Assets/Scripts/LightRotation/RotatingLight.cs
@@ -1,13 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
+using static UnityEngine.GraphicsBuffer;
 
 public class RotatingLight : MonoBehaviour
 {
+    private void Start()
+    {
+        Cursor.visible = false;
+    }
+
     public GameObject cam;
+    public Vector3 mousePos;
+    public float rotationSpeed;
     void Update()
     {
-        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        transform.rotation = Quaternion.LookRotation(Vector3.forward, mousePos - cam.transform.position);
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (mousePos != null)
+        {
+            RotateTowardsTarget();
+        }
+    }
+
+    void RotateTowardsTarget()
+    {
+        Vector3 targetDirection = mousePos - transform.position;
+        Quaternion targetRotation = Quaternion.LookRotation(transform.forward, targetDirection);
+
+        // Use Quaternion.Slerp to smoothly rotate towards the target
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
     }
 }
--- Justev's Passageway/Assets/Scripts/StalkerAi/StalkerAI.cs
+++ Justev's Passageway/Assets/Scripts/StalkerAi/StalkerAI.cs
@@ -2,13 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Pathfinding;
+using TMPro;
 
 public class StalkerAI : MonoBehaviour
 {
     public List<GameObject> waypoints;
     public GameObject playerTarget;
 
+    public Vector3 pathTargetPosition;
+
     public float speed;
+    public float rotSpeed;
     public float nextWaypointDistance;
 
     private Vector3 destination;
@@ -49,7 +53,9 @@ public class StalkerAI : MonoBehaviour
 
     // Update is called once per frame
     void FixedUpdate()
-    { 
+    {
+        Vector3 directionTopPathTarget = pathTargetPosition - rb.transform.position;
+
         // Goes To next Waypoint when at Waypoint
         if (path == null)
         {
@@ -65,20 +71,34 @@ public class StalkerAI : MonoBehaviour
         // when not at end of path
         else
         {
-            // Rotates light Towards waypoint
-            rb.transform.rotation = Quaternion.LookRotation(Vector3.forward, path.vectorPath[currentWaypoint] - rb.transform.position);
-
-            //Moves Stalker
-            Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;
-            Vector2 force = direction * speed * Time.deltaTime;
-            rb.AddForce(force);
-            float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
-
-            if (distance < nextWaypointDistance)
             {
-                currentWaypoint++;
+                pathTargetPosition = path.vectorPath[currentWaypoint];
+                Vector3 directionToTarget = pathTargetPosition - rb.transform.position;
+                directionToTarget.z = 0;
+
+                //Moves Stalker
+                Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;
+                Vector2 force = direction * speed * Time.deltaTime;
+                rb.AddForce(force);
+                float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
+
+                if (distance < nextWaypointDistance)
+                {
+                    currentWaypoint++;
+                }
+
+                //RotatesStalker
+                if (directionToTarget != Vector3.zero)
+                {
+                    // Calculate the rotation needed to face the target position
+                    Quaternion PathTargetRotation = Quaternion.LookRotation(rb.transform.forward, directionToTarget);
+
+                    // Rotate towards the target position with a specified speed
+                    rb.transform.rotation = Quaternion.RotateTowards(rb.transform.rotation, PathTargetRotation, rotSpeed * Time.deltaTime).normalized;
+                }
+
+                
             }
-            
         }
     }
 }
